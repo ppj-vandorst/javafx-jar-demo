1d40fff94cd238a4f5db1bcbb46c687568822be3
Pieter van Dorst <ppj.vandorst@gmail.com>
2021-08-25 15:28:14 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.demo3 to javafx.fxml;
+    exports com.example.demo3;
+}
